785e4558c3af278ab29edba3a6f918bbbf7d7336
Johann Fuechsl <johann@fuechsl.co>
2020-07-17 07:36:48 +0200

    Fix configuration and allow legacy token auth as well

--- rootfs/api/settings/production.py
+++ rootfs/api/settings/production.py
@@ -120,9 +120,11 @@ AUTHENTICATION_BACKENDS = (
 
 OIDC_RP_CLIENT_ID = os.environ['OIDC_RP_CLIENT_ID']
 OIDC_RP_CLIENT_SECRET = os.environ['OIDC_RP_CLIENT_SECRET']
+OIDC_RP_SIGN_ALGO = 'RS256'
 OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ["OIDC_AUTH_ENDPOINT"]
 OIDC_OP_TOKEN_ENDPOINT = os.environ["OIDC_TOKEN_ENDPOINT"]
 OIDC_OP_USER_ENDPOINT = os.environ["OIDC_USERINFO_ENDPOINT"]
+OIDC_OP_JWKS_ENDPOINT = os.environ["OIDC_OP_JWKS_ENDPOINT"]
 OIDC_RP_SCOPES = 'openid email profile'
 OIDC_RENEW_ID_TOKEN_EXPIRY_SECONDS = 600
 
@@ -159,6 +161,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # standard datetime format used for logging, model timestamps, etc.
 DEIS_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
+OIDC_DRF_AUTH_BACKEND = 'api.authentication.KYAuthOIDCBackend'
+
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': DEIS_DATETIME_FORMAT,
     'DEFAULT_MODEL_SERIALIZER_CLASS': 'rest_framework.serializers.ModelSerializer',
@@ -167,6 +171,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'mozilla_django_oidc.contrib.drf.OIDCAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
@@ -243,6 +248,11 @@ LOGGING = {
             'propagate': True,
         },
         'scheduler': {
+            'handlers': ['console'],
+            'level': LOG_LEVEL,
+            'propagate': True,
+        },
+        'mozilla_django_oidc': {
             'handlers': ['console'],
             'level': LOG_LEVEL,
             'propagate': True,
